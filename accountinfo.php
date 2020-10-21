e597703251cc3453517f29910df0aaa48abbb61e
mihirkj <41590002+mihirkj@users.noreply.github.com>
2020-10-22 00:26:00 +0530

    profile pic can now be changed from here

--- accountinfo.php
+++ accountinfo.php
@@ -257,6 +257,74 @@
           style="border-radius: 50%; box-shadow: 10px 10px 5px #ccc;"
         />
         <br>
+
+
+        <?php
+          function func() {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+          
+            if(isset($_POST["submit"])) {
+              $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+              if($check !== false) {
+              
+                $uploadOk = 1;
+              } else {
+            
+                $uploadOk = 0;
+              }
+            }
+
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+              echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+              $uploadOk = 0;
+            }
+
+          
+            if ($uploadOk == 0) {
+              //echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+              if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                //echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+              } else {
+               // echo "Sorry, there was an error uploading your file.";
+              }
+            }
+
+            //include_once('config.php');
+            $conn = mysqli_connect('localhost', 'root', 'root');
+            mysqli_select_db($conn, 'pharmeazy');
+
+            $curr_email = $_GET['id'];
+            //echo $_FILES["fileToUpload"]["name"];
+            $picurl = "uploads/".$_FILES["fileToUpload"]["name"];
+            $sql = "UPDATE `users` SET `piclink` = '$picurl' WHERE `users`.`email` = '$curr_email'; ";
+            $res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+            //$product = mysqli_fetch_array($res);
+            echo("<meta http-equiv='refresh' content='1'>");
+
+          }
+
+          if(isset($_POST['submit']))
+            {
+              func();
+            } 
+          ?>
+
+        <form action="" method="post" enctype="multipart/form-data">
+          <br>
+          <input type="file" name="fileToUpload" id="fileToUpload" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width: 20em;">
+          <br>
+          <br>
+          <input type="submit" value="Upload Image" name="submit">
+        </form>
+        
+        
         <br>
         <br>
         <h4 style="margin-left: -8%;" >My uploaded prescriptions:</h3>
